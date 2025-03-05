d626803557bc07155b7e4a39add2a0cfcc49c7af
Warmanel <huseynseyfullayev1234@gmail.com>
2025-03-05 18:11:32 +0300

    Style changed, new logic added, keyboard reader added.

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 const display = document.getElementById("inp");
-const answer = document.getElementById("res")
+const answer = document.getElementById("res");
+const numberKeys = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const operatorKeys = ['/', '+', '*', '-','.','×','÷'];
+const clearKeys = ['Backspace', 'Delete', 'Escape'];
+const enterKeys = ['Enter', '='];
 
 function appendToDisplay(input){
     if (display.textContent === '0') {
         display.textContent = input;
     } else {
         display.textContent += input;
+    }
+
+    if (z!=0){
+        display.textContent=input;
+        z=0;
+        answer.textContent="";
     }
 }
 
@@ -17,17 +27,48 @@ function clearDisplay(){
 function clearDisplayOne(){
     const currentContent = display.textContent;
     display.textContent = currentContent.slice(0, -1);
+    if (display.textContent===''){
+        display.textContent = 0;
+        answer.textContent ='';
+    }
 }
 
 function calculate(){
+    z=0;
     try {
         const expression = display.textContent
             .replace('×', '*')
             .replace('÷', '/');
         const result = eval(expression);
         answer.textContent = result;
+        z=1;
     } catch (error) {
         answer.textContent = 'Error';
     }
 
 }
+
+document.addEventListener('keydown', function(event) {
+    const key=event.key;
+    if (numberKeys.includes(key)){
+        appendToDisplay(key);
+    }
+
+    if (operatorKeys.includes(key)) {
+        const displayKey = key === '*' ? '×' : key === '/' ? '÷' : key;
+        appendToDisplay(displayKey);
+    }
+
+    if (clearKeys.includes(key)){
+        if(key==='Delete' || key==='Escape'){
+            clearDisplay();
+        }
+        if (key==='Backspace'){
+            clearDisplayOne();
+        }
+    }
+
+    if (enterKeys.includes(key)){
+        calculate();
+    }
+});
